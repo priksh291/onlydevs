042719b952e2f8efb47449939e9a02bbeca8b114
kshitij <kshitij@trulymadly.com>
2024-02-29 01:36:07 +0530

    globe width change

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import './App.css'
 import Home from './pages/home';
 import Navbar from './components/Navbar';
-import Search from './pages/search';
-import Aboutus from './pages/aboutus';
 
 
 function App() {
@@ -17,8 +15,6 @@ function App() {
       </div>
       <Routes>
         <Route path = '/' element = {<Home/>}/>
-        <Route path = '/search' element = {<Search/>}/>
-        <Route path = '/aboutus' element = {<Aboutus/>}/>
       </Routes>
 
     </Router>
